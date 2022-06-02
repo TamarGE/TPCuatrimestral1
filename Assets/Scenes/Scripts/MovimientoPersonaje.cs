487e4aac6b266b9d7242ece869690a9277d65d5e
GUERSON EMILIA <emiliaguerson@gmail.com>
2022-06-02 13:56:12 -0300

    Initial commit

---
+++ Assets/Scenes/Scripts/MovimientoPersonaje.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoPersonaje : MonoBehaviour
+{
+    public float movementSpeed;
+    public float rotationSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            //transform.position += new Vector3(0, 0, movementSpeed);
+            transform.Translate(0, 0, movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(0, 0, -movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(movementSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(-movementSpeed, 0, 0);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# TPCuatrimestral1
+ 
