2485cb4ec0bd599004c2990fa24a618c20963455
GUERSON EMILIA <emiliaguerson@gmail.com>
2022-06-10 15:31:50 -0300

    Botones3
    La configuración del texto y los botones de inicio está finalizada

--- Assets/Scenes/Scripts/BotonEmpezar.cs
+++ Assets/Scenes/Scripts/BotonEmpezar.cs
@@ -6,12 +6,16 @@ using UnityEngine.UI;
 public class BotonEmpezar : MonoBehaviour
 {
     public GameObject myObject;
+    public GameObject botonEmpezar;
+    public GameObject Instrucciones;
     public bool seVe = false;
     public void Ver()
     {
         if (seVe == false)
         {
             myObject.SetActive(!myObject.activeInHierarchy);
+            Instrucciones.SetActive(!Instrucciones.activeInHierarchy);
+            botonEmpezar.SetActive(!botonEmpezar.activeInHierarchy);
             seVe = true;
         }
     }
--- Assets/Scenes/Scripts/BotonSiguiente.cs
+++ Assets/Scenes/Scripts/BotonSiguiente.cs
@@ -7,6 +7,7 @@ public class BotonSiguiente : MonoBehaviour
     public GameObject textoInicial;
     public GameObject Instrucciones;
     public GameObject botonEmpezar;
+    public GameObject botonSeguir;
     public bool seVe = true;
     public void Ver()
     {
@@ -15,6 +16,7 @@ public class BotonSiguiente : MonoBehaviour
             textoInicial.SetActive(!textoInicial.activeInHierarchy);
             Instrucciones.SetActive(!Instrucciones.activeInHierarchy);
             botonEmpezar.SetActive(!botonEmpezar.activeInHierarchy);
+            botonSeguir.SetActive(!botonSeguir.activeInHierarchy);
             seVe = false;
         }
     }
